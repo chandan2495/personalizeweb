ce8fee82ee699dd6ce34e34fdc20539bf0814ad8
unknown <csingh@csingh.gp.cv.commvault.com>
2016-02-26 17:30:21 +0530

    wallpaper from unsplash.com

--- js/newTab.js
+++ js/newTab.js
@@ -1,6 +1,6 @@
 angular.module("myapp",[]).
 controller('maincontroller', ['$scope', function($scope){
-	$scope.lastOpenedUrls=[];
+	$scope.lastOpenedUrls=[];	
 	$scope.getUrlFromLocalStorage=function(){
 		$scope.lastOpenedUrls = JSON.parse(localStorage.getItem("lastOpenedUrls"));	
 	};
@@ -8,6 +8,11 @@ controller('maincontroller', ['$scope', function($scope){
 		angular.forEach($scope.lastOpenedUrls,function(url,index){
 			chrome.tabs.create({ url: url.url });
 		});
+	};
+	$scope.changeWallpaper = function(){		
+		var currentTime=(new Date()).getTime();
+		var random = parseInt(Math.random()*100000);
+		$('html').css('background',"url(https://source.unsplash.com/category/nature/1920x1080?a="+ currentTime + "a"+ random +") no-repeat center center fixed");
 	};
 
 	$scope.getUrlFromLocalStorage();
