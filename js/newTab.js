fec991e427a5fe2448fd5bd70854cccb0303316a
Chandan Singh <csingh@CommVault.com>
2016-05-03 17:08:39 +0530

    storing image in localstorage

--- js/newTab.js
+++ js/newTab.js
@@ -1,5 +1,5 @@
 angular.module("myapp",[]).
-controller('maincontroller', ['$scope', function($scope){
+controller('maincontroller', ['$scope','$http', '$q', function($scope, $http, $q) {
 	$scope.lastOpenedUrls=[];	
 	$scope.getUrlFromLocalStorage=function(){
 		$scope.lastOpenedUrls = JSON.parse(localStorage.getItem("lastOpenedUrls"));	
@@ -9,11 +9,48 @@ controller('maincontroller', ['$scope', function($scope){
 			chrome.tabs.create({ url: url.url });
 		});
 	};
-	$scope.changeWallpaper = function(){		
-		var currentTime=(new Date()).getTime();
-		var random = parseInt(Math.random()*100000);
-		$('html').css('background',"url(https://source.unsplash.com/category/nature/1920x1080?a="+ currentTime + "a"+ random +") no-repeat center center fixed");
+	// $scope.changeWallpaper = function(){		
+	// 	var currentTime=(new Date()).getTime();
+	// 	var random = parseInt(Math.random()*100000);
+	// 	$http.get("https://source.unsplash.com/category/nature/1024x768").then(function(data){
+	// 		console.log(btoa(data));
+	// 	}, function(error) {
+	// 		console.log(error.data);
+	// 	});
+	// 	$('html').css('background',"url(https://source.unsplash.com/category/nature/1920x1080?a="+ currentTime + "a"+ random +") no-repeat center center fixed");
+	// };
+
+	$scope.getImageData = function() {
+		var url = "https://source.unsplash.com/category/nature/1920x1080";
+		var img = new Image();			
+	    img.crossOrigin = 'Anonymous';
+	    var deferred = $q.defer();
+	    img.onload = function(){
+	        var canvas = document.createElement('CANVAS');
+	        var ctx = canvas.getContext('2d');
+	        canvas.height = this.height;
+	        canvas.width = this.width;
+	        ctx.drawImage(this, 0, 0);
+	        dataURL = canvas.toDataURL('jpeg');	        
+	        canvas = null; 	    
+	        deferred.resolve(dataURL);   
+	    };
+	    img.src = url;
+	    return deferred.promise;
 	};
 
+	$scope.setBackgroundImage = function() {
+		var lastBackgroundImage = localStorage.getItem('lastBackgroundImage');		
+		if(lastBackgroundImage) {
+			$('html').css('background',"url("+ lastBackgroundImage +") no-repeat center center fixed");
+		}		
+		$scope.getImageData().then(function(data){
+			if(!lastBackgroundImage)
+				$('html').css('background',"url("+ data +") no-repeat center center fixed");			
+			localStorage.setItem('lastBackgroundImage', data);			
+		});							
+	};
+
+	$scope.setBackgroundImage();
 	$scope.getUrlFromLocalStorage();
 }]);
--- newTab.js
+++
@@ -1,9 +0,0 @@
-angular.module("myapp",[]).
-controller('maincontroller', ['$scope', function($scope){
-	$scope.lastOpenedUrls=[];
-	$scope.getUrlFromLocalStorage=function(){
-		$scope.lastOpenedUrls = JSON.parse(localStorage.getItem("lastOpenedUrls"));	
-	};
-
-	$scope.getUrlFromLocalStorage();
-}]);
